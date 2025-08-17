cbd3064d468145bb381ea6ae744c652cb234022c
acharya100 <drabeshacharya@gmail.com>
2025-08-18 00:05:13 +0545

    Added files

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    author=models.CharField(max_length=120,blank=True,null=True)
 
     def __str__(self):
         return self.title
