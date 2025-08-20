77b212e9a3da1aded676483ee8ca9ce13b8ab92e
acharya100 <drabeshacharya@gmail.com>
2025-08-20 14:09:30 +0545

    new

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_at=models.DateTimeField(auto_now=True,blank=True,null=True)
     published_date = models.DateTimeField(auto_now_add=True)
     author=models.CharField(max_length=120,blank=True,null=True)
 
